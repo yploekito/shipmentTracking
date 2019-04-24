55885d2978a8abb60c85d551844af7e54b1cd877
yploekito <yp.loekito@gmail.com>
2019-04-24 17:13:03 +0700

    added app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const homeRoute = require('./routes/homeRoute')
+
+app.use(express.urlencoded({extended:false}))
+app.listen(port, ()=>{
+    console.log(`connected to port ${port}`)
+})
+
+app.use('/', homeRoute)
+
