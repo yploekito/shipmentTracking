cb50993d75d0db991380da583dfc125a6afbf4bf
Stephen Lie <stephenlie55@yahoo.com>
2019-04-25 21:40:17 +0700

    testingisbelieving

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const homeRoute = require('./router/homeRoute')
+const sequelize = require('sequelize');
 
 app.use(express.urlencoded({extended:false}))
 app.listen(port, ()=>{
